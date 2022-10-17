adc877f23c1917c7caa87ddba26e7e0ef41564b6
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-17 20:30:17 +0100

    Add new fields to signup form

--- account/forms.py
+++ account/forms.py
@@ -1,19 +1,26 @@
 from django import forms
 from allauth.account.forms import SignupForm, LoginForm
 
+ACCOUNT_TYPE = (
+    ('eld', 'Elderly Member'),
+    ('vol', 'Volunteer'),
+)
+
 class UserSignUpForm(SignupForm):
     first_name = forms.CharField(max_length=155)
     last_name = forms.CharField(max_length=155)
+    account_type = forms.ChoiceField(choices=ACCOUNT_TYPE)
     tel = forms.CharField(max_length=20)
 
     def __init__(self, *args, **kwargs):
         super(UserSignUpForm, self).__init__(*args, **kwargs)
-
-        del self.fields['password1']
-        del self.fields['password2']
+        
+        del self.fields['email'].widget.attrs['placeholder']
+        del self.fields['password1'].widget.attrs['placeholder']
+        del self.fields['password2'].widget.attrs['placeholder']
 
         self.fields['first_name'].widget.attrs.update({
-            'class': 'required',
+            'class': 'required input-splt',
         })
         self.fields['last_name'].widget.attrs.update({
             'class': 'required',
@@ -24,11 +31,15 @@ class UserSignUpForm(SignupForm):
         self.fields['tel'].widget.attrs.update({
             'class': 'required',
         })
+        self.fields['account_type'].widget.attrs.update({
+            'class': 'required', 'type':'radio',
+        })
 
     def custom_signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.tel_number = self.cleaned_data['tel']
+        user.account_types = self.cleaned_data['account_type']
         user.save()
 
 class UserLoginForm(LoginForm):
