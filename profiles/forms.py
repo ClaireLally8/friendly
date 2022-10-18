204e619b64beab87df772379e2205a7b51458dc2
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-18 19:31:06 +0100

    Complete signup

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,26 +1,38 @@
 from django import forms
 from allauth.account.forms import SignupForm, LoginForm
-from .models import UserProfile
+from .models import UserProfile, UserType
 
-class LoginForm(LoginForm):
+ACCOUNT_TYPE = (
+    ('eld', 'Elderly Member'),
+    ('vol', 'Volunteer'),
+)
+
+class UserLoginForm(LoginForm):
 
     def __init__(self, *args, **kwargs):
-        super(LoginForm, self).__init__(*args, **kwargs)
+        super(UserLoginForm, self).__init__(*args, **kwargs)
         del self.fields['login'].widget.attrs['placeholder']
         del self.fields['password'].widget.attrs['placeholder']
         self.fields['login'].widget = forms.TextInput(attrs={'class': 'input-wide'})
         self.fields['password'].widget = forms.PasswordInput(attrs={'class': 'input-wide'})
 
-class SignUpForm(SignupForm):
+class UserSignUpForm(SignupForm):
     first_name = forms.CharField(max_length=155)
     last_name = forms.CharField(max_length=155)
+    phone_number = forms.CharField(max_length=30)
+    account_type = forms.ChoiceField(choices=ACCOUNT_TYPE)
 
     def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
+        super(UserSignUpForm, self).__init__(*args, **kwargs)
         
         del self.fields['email'].widget.attrs['placeholder']
+        del self.fields['username'].widget.attrs['placeholder']
         del self.fields['password1'].widget.attrs['placeholder']
         del self.fields['password2'].widget.attrs['placeholder']
+
+        self.fields['username'].widget.attrs.update({
+            'class': 'required input-wide',
+        })
 
         self.fields['first_name'].widget.attrs.update({
             'class': 'required  input-wide',
@@ -47,9 +59,15 @@ class SignUpForm(SignupForm):
     def custom_signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
+        user.save()
 
         profile = UserProfile()
+        userType = UserType()
+        profile.user = user
         profile.phone_number = self.cleaned_data['phone_number']
-        profile.account_types = self.cleaned_data['account_type']
         profile.save()
-        user.save()
+        userType.user = user
+        userType.account_type = self.cleaned_data['account_type']
+        userType.save()
+        
+        
--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 ACCOUNT_TYPE = (
@@ -9,16 +10,20 @@ ACCOUNT_TYPE = (
 )
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE)
-    account_type = models.CharField(choices=ACCOUNT_TYPE,max_length=200, null=False, blank=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     phone_number = models.CharField(max_length=30, null=False)
     postcode = models.CharField(max_length=500, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=False)
-    street_address1 = models.CharField(max_length=80, null=False)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+class UserType(models.Model):
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    account_type = models.CharField(choices=ACCOUNT_TYPE,max_length=200, null=False, blank=False)
 
     def __str__(self):
         return self.user.username
