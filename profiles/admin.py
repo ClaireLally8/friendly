6b4a547d3a41ca74a264254c96d0d033bf1590d1
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-18 19:30:24 +0100

    added user type to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserProfile
+from .models import UserProfile, UserType
 
 admin.site.register(UserProfile)
+admin.site.register(UserType)
