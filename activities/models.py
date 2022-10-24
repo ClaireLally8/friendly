3126172464b783e8a7e719c979434cef076e8f3b
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-24 20:27:26 +0100

    Added autofill functionality to models

--- activities/models.py
+++ activities/models.py
@@ -1,4 +1,3 @@
-from email.policy import default
 import uuid
 from django.db import models
 from django.conf import settings
@@ -19,14 +18,35 @@ class Activity(models.Model):
     description = models.CharField(max_length=140, null=False, blank=False)
     available = models.BooleanField(default=True)
     
-    def _generate_order_number(self):
+    def _generate_activity_id(self):
         return uuid.uuid4().hex.upper()
+
+    def _generate_activity_duration(self):
+        return self.end_time - self.start_time
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the activity ID and 
+        activity duration if it hasn't been set already.
+        """
+        if not self.activity_id:
+            self.activity_id = self._generate_activity_id()
+        super().save(*args, **kwargs)
+
+        if not self.duration:
+            self.duration = self._generate_activity_duration()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.activity_id
 
 class Request(models.Model):
     activity = models.ForeignKey(Activity, null=False, blank=False, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     accepted = models.BooleanField(default=False)
     
+    def __str__(self):
+        return self.activity.activity_id
 
 
 
