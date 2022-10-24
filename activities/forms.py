a1bd0676281ea59b9d41b3af5b42187bec8a9b59
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-24 20:16:23 +0100

    Creat new activity form

---
+++ activities/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Activity
+
+class ActivityForm(forms.ModelForm):
+    class Meta:
+        model = Activity
+        fields = ('host', 'name', 'date',
+                  'start_time', 'end_time',
+                  'duration', 'location', 'description')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Activity Type',
+            'date': 'Date of Activity',
+            'start_time': 'Start Time',
+            'end_time': 'End Time',
+            'duration': 'Duration',
+            'location': 'Location',
+            'description': 'Description',
+            'host': 'Host',
+        }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
