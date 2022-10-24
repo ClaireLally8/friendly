b8eda2cf2a2ef2fada4e4be83d424519ff387b59
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-24 20:39:39 +0100

    Add filter for today

--- activities/views.py
+++ activities/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .forms import ActivityForm
 from .models import Activity, Request
+
+from datetime import date
 # Create your views here.
 def activities(request):
     form = ActivityForm()
     activities = Activity.objects.all()
+    today= date.today()
 
     context = {
+        'today':today,
         'form': form,
         'activities': activities
     }
