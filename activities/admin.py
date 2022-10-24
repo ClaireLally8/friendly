440902da9f4280d0399bd1089b2bdc916e6af8fb
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-24 18:45:41 +0100

    Create activity models

--- activities/admin.py
+++ activities/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Activity, Request
+
+admin.site.register(Activity)
+admin.site.register(Request)
--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,34 @@
+from email.policy import default
+import uuid
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Activity(models.Model):
+    class Meta:
+        verbose_name_plural = 'Activities'
+
+    activity_id = models.CharField(max_length=32, null=False, editable=False)
+    host = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    name = models.CharField(max_length=254, null=False, blank=False)
+    date = models.DateField()
+    start_time =models.TimeField()
+    end_time = models.TimeField()
+    duration = models.DurationField()
+    location = models.CharField(max_length=40, null=False, blank=False)
+    description = models.CharField(max_length=140, null=False, blank=False)
+    available = models.BooleanField(default=True)
+    
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+class Request(models.Model):
+    activity = models.ForeignKey(Activity, null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    accepted = models.BooleanField(default=False)
+    
+
+
+
+
+
