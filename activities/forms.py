5d97218919c6a52cf51d2b6882f97d02a092d7c8
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-27 20:49:37 +0100

    remove unused code

--- activities/forms.py
+++ activities/forms.py
@@ -30,5 +30,4 @@ class ActivityForm(forms.ModelForm):
             else:
                 placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
