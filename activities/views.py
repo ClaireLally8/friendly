9bad0449ed8dc3c6c1fe1ca6863be628c3cf3968
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-27 20:51:17 +0100

    update template and views for new activity

--- activities/views.py
+++ activities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 
 from .forms import ActivityForm
@@ -10,13 +10,22 @@ def activities(request):
     form = ActivityForm()
     activities = Activity.objects.all()
     today= date.today()
-    paginator = Paginator(activities, 3)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
 
     context = {
-        'page_obj': page_obj,
+        'page_obj': activities,
         'today':today,
         'form': form,
     }
     return render(request, 'activities/activities_overview.html', context)
+
+def create_activity(request):
+    if request.method == "POST":
+        form = ActivityForm(request.POST)
+        form.save()
+        return redirect('activities')
+    else:
+        form = ActivityForm()
+        context = {
+        'form': form,
+    }
+        return render(request, 'activities/new_listing.html', context)
