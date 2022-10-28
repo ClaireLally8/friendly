ab887316298f501e146a23060d45fb41a2c392b0
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-10-28 17:36:35 +0100

    Create custom form functionality

--- activities/forms.py
+++ activities/forms.py
@@ -4,30 +4,14 @@ from .models import Activity
 class ActivityForm(forms.ModelForm):
     class Meta:
         model = Activity
-        fields = ('host', 'name', 'date',
+        fields = ('name', 'date',
                   'start_time', 'end_time', 
                   'location', 'description')
-
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
-        super().__init__(*args, **kwargs)
-        placeholders = {
+        labels = {
             'name': 'Activity Type',
             'date': 'Date of Activity',
             'start_time': 'Start Time',
             'end_time': 'End Time',
             'location': 'Location',
             'description': 'Description',
-            'host': 'Host',
         }
-
-        for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].label = False
