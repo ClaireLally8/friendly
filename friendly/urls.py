fe37e7dbe8ba3ee4732c9b493d7b693006b85902
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-11-26 22:40:02 +0000

    Created profile functionality

--- friendly/urls.py
+++ friendly/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('activities/', include('activities.urls')),
+    path('profile/', include('profiles.urls')),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from allauth import Users
+from .models import UserProfile, UserType
+
+def profile(request):
+    user = request.user
+    account = Users.objects.filter(username=user).values()
+    profile = UserProfile.objects.filter(user=user).values()
+    user_type = UserType.objects.filter(user=user).values()
+
+    context = {
+        'user':user,
+        'profile':profile,
+        'user_type': user_type
+    }
+    return render(request, 'profiles/profile_overview.html', context)
