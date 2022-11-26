2c196e1d08fb8d214ca7882a62b8f36d1e4b04ee
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-11-26 23:34:23 +0000

    Updated views for user profile

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.models import User
 
 from .models import UserProfile, UserType
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -11,9 +12,17 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=user)
     user_type = get_object_or_404(UserType, user=user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserProfileForm(instance=profile)
+
     context = {
         'user':user,
         'profile':profile,
-        'user_type': user_type
+        'user_type': user_type,
+        'form' : form
     }
     return render(request, 'profiles/profile_overview.html', context)
