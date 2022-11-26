1c756ac3e9bafd223007a67e666c9e29836a3320
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-11-26 23:24:36 +0000

    Updated settings to allow images to be uploaded

--- friendly/settings.py
+++ friendly/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -144,5 +145,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
