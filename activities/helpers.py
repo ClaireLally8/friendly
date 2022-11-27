26247e2b519cb636d4f339e3c894497a8f360285
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-11-27 22:39:34 +0000

    Crete helper function to get account type and user profile

--- activities/helpers.py
+++ activities/helpers.py
@@ -1,7 +1,13 @@
-from profiles.models import UserProfile
+from profiles.models import UserProfile, UserType
 
-def get_userprofile(request):
-    profile = UserProfile.objects.filter(user=request.user)
+def get_userprofile(request, user):
+    profile = UserProfile.objects.filter(user=user)
     if profile.exists():
         return profile.first()
+    return None
+
+def get_usertype(request, user):
+    account = UserType.objects.filter(user=user)
+    if account.exists():
+        return account.first()
     return None
