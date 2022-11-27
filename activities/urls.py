fd70e96dadaa8db5a9a58fab6c23d74a53f67563
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-11-27 18:09:25 +0000

    Set up view activity funcitonality

--- activities/urls.py
+++ activities/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.activities, name='activities'),
+    path('<int:id>', views.view_activity, name='view-activity'),
     path('new/', views.create_activity, name='create-activity'),
     path('edit/<int:id>', views.edit_activity, name='edit-activity'),
     path('delete/<int:id>', views.delete_activity, name='delete-activity'),
