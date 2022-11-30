f8f18af98612c06779938ced8d163474e1616947
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-11-30 23:15:22 +0000

    add allauth

--- friendly/urls.py
+++ friendly/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('activities/', include('activities.urls')),
     path('profile/', include('profiles.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
