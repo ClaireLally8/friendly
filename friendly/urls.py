93f16f2267ac7d45719aefbd4a96049ccbfdcdd3
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-12-04 21:56:36 +0000

    Create main pages

--- friendly/urls.py
+++ friendly/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', include('main.urls')),
     path('activities/', include('activities.urls')),
     path('request/', include('activity_requests.urls')),
     path('profile/', include('profiles.urls')),
